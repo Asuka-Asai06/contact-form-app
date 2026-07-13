9dd12bdbde7775106d50ff6217336ceb0506f74a
Asuka Asai <asaiasuka06@gmail.com>
2026-07-13 23:20:35 +0900

    feat: エクスポート機能のテストの実装 #10

--- app/Http/Requests/ExportContactRequest.php
+++ app/Http/Requests/ExportContactRequest.php
@@ -7,9 +7,6 @@ use Illuminate\Validation\Rule;
 
 class ExportContactRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
